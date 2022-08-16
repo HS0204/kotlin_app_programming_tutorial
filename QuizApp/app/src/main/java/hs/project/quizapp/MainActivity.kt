c1247098ca569cce056794bd3fff267741736908
Hanseul Lee <lhanseul0204@naver.com>
2022-08-17 00:28:13 +0900

    3) 액티비티 생명주기

--- QuizApp/app/src/main/java/hs/project/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/hs/project/quizapp/MainActivity.kt
@@ -2,6 +2,7 @@ package hs.project.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(binding.root)
 
         trueBtn = binding.trueBtn
@@ -52,6 +54,31 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTxtResId = questionBank[currentIndex].textResId
         questionTxtView.setText(questionTxtResId)
@@ -67,5 +94,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
